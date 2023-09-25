5e93645607c0de3b905aef35194dff3919098c9a
Nguyễn Chí Công <95849309+conggaro@users.noreply.github.com>
2023-09-25 14:30:08 +0700

    Update app.component.ts

--- Sử dụng FormsModule/src/app/app.component.ts
+++ Sử dụng FormsModule/src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'DuAn_02';
-}
+export class AppComponent {}
